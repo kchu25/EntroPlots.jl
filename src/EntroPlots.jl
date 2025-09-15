c859ca6367d30ed253faacc103e8a135d12b1cb3
Shane Chu <skchu@wustl.edu>
2025-09-14 23:53:19 -0400

    update

--- src/EntroPlots.jl
+++ src/EntroPlots.jl
@@ -5,29 +5,29 @@ using Plots
 include("const.jl")
 include("const_glyphs.jl")
 include("helpers.jl")
-include("helpers_shape.jl")
-include("helpers_spacers.jl")
-include("plot_logo.jl")
-include("plot_logo_w_crosslinks.jl")
+
 include("plot_nothing.jl")
-include("logo_rect_helpers.jl")
+include("plot_logo.jl")
+# include("helpers_shape.jl")
+# include("helpers_spacers.jl")
+# include("plot_logo_w_crosslinks.jl")
+# 
+# include("logo_rect_helpers.jl")
 include("plot_logo_w_arr_gaps.jl")
 
 
-export LogoPlot,
-    logoplotwithcrosslink,
+export logoplot,
     logoplot_with_highlight,
-    logoplot_with_highlight_crosslink,
     save_logoplot,
-    save_crosslinked_logoplot,
-    logoplot_with_arrow_gaps,
-    save_logo_w_arrows,
-    reduce_entropy!,
-    NothingLogo,
-    get_rectangle_basic,
-    logoplot_with_rect_gaps
-
-
-
+    reduce_entropy!
+# export LogoPlot,
+#     logoplotwithcrosslink,
+#     logoplot_with_highlight_crosslink,
+#     save_crosslinked_logoplot,
+#     logoplot_with_arrow_gaps,
+#     save_logo_w_arrows,
+#     NothingLogo,
+#     get_rectangle_basic,
+#     logoplot_with_rect_gaps
 
 end
